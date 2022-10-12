195144b7bfffd7b47a5892851c1fb957d176e524
Monster-Mohamed <ma8494927@gmail.com>
2022-10-12 08:24:58 +0200

    * Finish the User * Finish the Point * Finish the Authentication and the Guards, Middlewares * Finish the relations between them

--- server/src/app/modules/image.module.ts
+++ server/src/app/modules/image.module.ts
@@ -1,10 +1,9 @@
 import { Module } from "@nestjs/common";
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [TypeOrmModule.forFeature([])], // Entities
   controllers: [], // Controllers
   providers: [], // Services
-  exports: [], // exports the services to another files
 })
 export class ImageModule {}
